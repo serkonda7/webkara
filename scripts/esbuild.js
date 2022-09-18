2f785a661be88636013ba94f4ea87e2746435822
Lukas Neubert <serkonda7@pm.me>
2022-09-18 20:56:41 +0200

    use tsc for dev builds; add CI

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,8 +1,6 @@
 import * as esbuild from 'esbuild'
 import process from 'process'
 
-const dev_build = process.argv.includes('--dev')
-
 const config = {
 	entryPoints: [
 		'src/main.ts',
@@ -11,9 +9,6 @@ const config = {
 		'src/editor.ts',
 	],
 	outdir: 'public/js',
-	minify: !dev_build,
-	sourcemap: dev_build,
-	watch: process.argv.includes('--watch'),
 }
 
 esbuild.build(config).catch(() => { process.exit(1) })
