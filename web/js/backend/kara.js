6e73cb6169aa109aac951d2124d638a2e4b39dbe
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2025-04-16 18:16:48 +0200

    mock backend methods

--- web/js/backend/kara.js
+++ web/js/backend/kara.js
@@ -1,4 +1,59 @@
 class KaraBackend {
+	move() {
+		throw new Error('move() not implemented')
+	}
+
+	turn_left() {
+		throw new Error('turn_left() not implemented')
+	}
+
+	turn_right() {
+		throw new Error('turn_right() not implemented')
+	}
+
+	put_leaf() {
+		throw new Error('put_leaf() not implemented')
+	}
+
+	take_leaf() {
+		throw new Error('take_leaf() not implemented')
+	}
+
+	is_tree_front() {
+		throw new Error('is_tree_front() not implemented')
+	}
+
+	is_tree_left() {
+		throw new Error('is_tree_left() not implemented')
+	}
+
+	is_tree_right() {
+		throw new Error('is_tree_right() not implemented')
+	}
+
+	is_on_leaf() {
+		throw new Error('is_on_leaf() not implemented')
+	}
+
+	is_mushroom_front() {
+		throw new Error('is_mushroom_front() not implemented')
+	}
+
+	set_position(x, y) {
+		throw new Error('set_position() not implemented')
+	}
+
+	get_position() {
+		throw new Error('get_position() not implemented')
+	}
+
+	set_rotation(dir) {
+		throw new Error('set_rotation() not implemented')
+	}
+
+	get_rotation() {
+		throw new Error('get_rotation() not implemented')
+	}
 }
 
 const _kara = new KaraBackend()
--- web/js/backend/tools.js
+++ web/js/backend/tools.js
@@ -1,4 +1,31 @@
 class ToolsBackend {
+	println(msg) {
+		throw new Error('println() not implemented')
+	}
+
+	show_popup(msg) {
+		throw new Error('show_popup() not implemented')
+	}
+
+	check_run_state() {
+		throw new Error('check_run_state() not implemented')
+	}
+
+	sleep(ms) {
+		throw new Error('sleep() not implemented')
+	}
+
+	string_input(msg) {
+		throw new Error('string_input() not implemented')
+	}
+
+	int_input(msg) {
+		throw new Error('int_input() not implemented')
+	}
+
+	float_input(msg) {
+		throw new Error('float_input() not implemented')
+	}
 }
 
 const _tools = new ToolsBackend()
