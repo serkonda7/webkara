5b0f3223354d423ce9613e397f853022228ac219
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2025-04-18 15:47:23 +0200

    implement changing grid size

--- web/js/frontend/main.js
+++ web/js/frontend/main.js
@@ -1,10 +1,58 @@
+import { b_world } from "../backend/world.js"
 import * as f_world from "./f_world.js"
+
+const btn_run = document.querySelector('#run-btn')
+const btn_pause = document.querySelector('#pause-btn')
+const btn_stop = document.querySelector('#stop-btn')
+
+const btn_apply_world_size = document.querySelector('#apply-world-size')
+const inp_world_width = document.querySelector('#world-width')
+const inp_world_height = document.querySelector('#world-height')
+
+const btn_obj_kara = document.querySelector('#obj-kara')
+const btn_obj_leaf = document.querySelector('#obj-leaf')
+const btn_obj_tree = document.querySelector('#obj-tree')
+const btn_obj_shroom = document.querySelector('#obj-shroom')
+const btn_obj_trash = document.querySelector('#obj-trash')
+
+function not_implemented() {
+	alert('Not implemented')
+}
 
 function main() {
 	f_world.draw_empty_grid()
 
-	// TODO world size input functionality
-	// TODO sync world size with input values
+	btn_run.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_pause.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_stop.addEventListener('click', () => {
+		not_implemented()
+	})
+
+	btn_apply_world_size.addEventListener('click', () => {
+		b_world.size.width = parseInt(inp_world_width.value)
+		b_world.size.height = parseInt(inp_world_height.value)
+		f_world.draw_empty_grid()
+	})
+
+	btn_obj_kara.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_obj_leaf.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_obj_tree.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_obj_shroom.addEventListener('click', () => {
+		not_implemented()
+	})
+	btn_obj_trash.addEventListener('click', () => {
+		not_implemented()
+	})
 }
 
 main()
