9ee7e296f6c60fa0f868b9c9127276b9d00c518e
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2025-04-20 13:14:47 +0200

    change cursor to selected obj

--- web/js/frontend/edit_mode.js
+++ web/js/frontend/edit_mode.js
@@ -4,6 +4,8 @@ const btn_obj_tree = document.querySelector('#obj-tree')
 const btn_obj_shroom = document.querySelector('#obj-shroom')
 const btn_obj_trash = document.querySelector('#obj-trash')
 
+const world_grid = document.querySelector('#world-grid')
+
 function init_click_listeners() {
 	btn_obj_kara.addEventListener('click', edit_mode)
 	btn_obj_leaf.addEventListener('click', edit_mode)
@@ -13,31 +15,29 @@ function init_click_listeners() {
 }
 
 let edit_mode_active = false
-let edit_mode_obj = null
 let edit_mode_selected = null
 
 function edit_mode(ev) {
 	const target = ev.currentTarget
 
 	if (edit_mode_active) {
+		// Unselect current object button
+		edit_mode_selected.classList.remove('selected')
+		world_grid.classList.remove(`cursor-${edit_mode_selected.dataset.obj}`)
+
 		// Disable edit mode
-		if (edit_mode_obj == target.dataset.obj) {
+		if (edit_mode_selected.dataset.obj == target.dataset.obj) {
 			edit_mode_active = false
-			edit_mode_obj = null
-			edit_mode_selected.classList.remove('selected')
 			edit_mode_selected = null
 			return
 		}
-
-		// Switch object: Unselect previous button
-		edit_mode_selected.classList.remove('selected')
 	}
 
-	// Enable edit mode
+	// Enable edit mode or switch selected object
 	edit_mode_active = true
-	edit_mode_obj = target.dataset.obj
 	edit_mode_selected = target
 
+	world_grid.classList.add(`cursor-${target.dataset.obj}`)
 	target.classList.add('selected')
 }
 
